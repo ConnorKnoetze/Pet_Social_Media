fb0a545f1808fc4b346ed47eb0851c8f864b0e6f
connorknoetze <connorknoetze@gmail.com>
2025-11-18 18:27:07 +1300

    chore(app) Setup domainmodel skeleton

---
+++ pets/domainmodel/AnimalType.py
@@ -0,0 +1,10 @@
+from enum import Enum
+class AnimalType(Enum):
+    DOG = "Dog"
+    CAT = "Cat"
+    BIRD = "Bird"
+    FISH = "Fish"
+    RABBIT = "Rabbit"
+    HAMSTER = "Hamster"
+    REPTILE = "Reptile"
+    OTHER = "Other"
